d3a91935a43e9b989ef1fefec20591a90c67738f
Paul Thibaul <Paulthb1@gmail.com>
2020-09-29 18:11:39 +0200

    update timeline + add gears script

---
+++ Assets/Scripts/Paul/Timeline/Gears.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gears : MonoBehaviour
+{
+    /**
+     * accès au gameobject enfant
+     */
+    public Animation gear = null;
+    public Image logo = null;
+
+    public void MoveGears()
+    {
+        StartCoroutine(GoToLeft());
+        print("bolosse");
+    }
+
+    public IEnumerator GoToLeft()
+    {
+        float timeToMove = 2;
+        float elapsedTime = 0;
+
+        Vector3 currentPos = transform.position;
+        Vector3 Gotoposition = transform.position + (Vector3.left * 200);
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(currentPos, Gotoposition, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = Gotoposition;
+    }
+}
